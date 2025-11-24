f513b8d3ac0e60a8a580eb94ced75d02f6376189
Joocheol <joocheol@yonsei.ac.kr>
2025-11-24 02:03:46 +0000

    .

--- hello_manim.py
+++ hello_manim.py
@@ -2,6 +2,8 @@ from manim import *
 
 class HelloManim(Scene):
     def construct(self):
-        text = Tex("Hello, $x+1$ Manim in Codespaces!")
+        text = Tex("Hello, $x+1$ Manim in Codespaces! $x$")
         self.play(Write(text))
         self.wait(1)
+
+        
